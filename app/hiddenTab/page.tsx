012362127d7dfbf74066a3f38898ba63bce6f08b
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-18 22:57:19 -0500

    icon update

--- app/hiddenTab/page.tsx
+++ app/hiddenTab/page.tsx
@@ -1,7 +1,7 @@
 export default function Secret() {
     return(
         <div>
-            <img src="/loadingScreen.png" style={{height: "15%", width: "15%"}}/>
+            <p>Insert something cool!</p>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-   <div style={{display: "flex", flexDirection: "column"}}>
-      <p>Yo Waddup Chain</p>
-      <Link href="/resume">Resume</Link>
-      <Link href="/hiddenTab">Link to what is going to be cool little side thing!</Link>
-      <img src="/loadingScreen.png" style={{height: "15%", width: "15%"}}/>
-   </div>
+    <div>
+      <div style={{display: "flex", flexDirection: "column"}}>
+          <p>Yo Waddup Chain</p>
+          <Link href="/resume">Resume</Link>
+          <Link href="/hiddenTab">Link to what is going to be cool little side thing!</Link>
+          <img src="/loadingScreen.png" style={{height: "15%", width: "15%"}}/>
+      </div>
+    </div>
   );
 }
