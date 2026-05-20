da8f48cf9b639efe18a5f2349a47cc9a23a65e89
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-20 16:29:16 -0500

    Custom Download Button on Resume Page
     - It needs to look better, I will get criticism and change it

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,14 +1,13 @@
 export default function Resume() {
     return(
-        // TODO Put a custom download button here!
         <div style={{height: "100%", backgroundColor: "#282828"}}>
-            <a href="/resumeMarch2026.pdf" download className="download-btn"
+            <a href="/Leo_Teeuwen_Resume.pdf" download className="download-btn left-12/16"
                 style={{ position: "absolute", top: 0, zIndex: 10 }}
             >
-                Download Now
+                <img src="/download.svg" style={{width: "50%"}}/>
             </a>
             <iframe
-                src="/resumeMarch2026.pdf#toolbar=0"
+                src="/Leo_Teeuwen_Resume.pdf#toolbar=0"
                 style={{ width: '100%', height: '100vh', border: 'none' }} 
                 title="Resume PDF March 2026"
             />
