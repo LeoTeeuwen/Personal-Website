68a099290e35441bf5413749bbc751ccb2c573fb
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-05-20 15:57:56 -0500

    basic resume download button

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,12 @@
 export default function Resume() {
     return(
         // TODO Put a custom download button here!
-        <div style={{height: "100%"}}>
+        <div style={{height: "100%", backgroundColor: "#282828"}}>
+            <a href="/resumeMarch2026.pdf" download className="download-btn"
+                style={{ position: "absolute", top: 0, zIndex: 10 }}
+            >
+                Download Now
+            </a>
             <iframe
                 src="/resumeMarch2026.pdf#toolbar=0"
                 style={{ width: '100%', height: '100vh', border: 'none' }} 
