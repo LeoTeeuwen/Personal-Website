00e8f7b642e6a42ed17a1735396702118535a69a
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-08-23 21:54:31 -0500

    Basic plane floor

--- app/components/PlayCanvasView.tsx
+++ app/components/PlayCanvasView.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { Application, Entity } from '@playcanvas/react';
-import { Camera, Light, Render, Script } from '@playcanvas/react/components';
-import { useApp, useAppEvent } from '@playcanvas/react/hooks';
+import { Camera, Collision, Light, Render, RigidBody, Script } from '@playcanvas/react/components';
+import { useApp, useAppEvent, useMaterial } from '@playcanvas/react/hooks';
 import type { Entity as PcEntity } from 'playcanvas';
 import { useRef } from 'react';
 import * as pc from 'playcanvas';
@@ -22,32 +22,35 @@ function Scene() {
   const userInputObject: { [key: string]: boolean } = {"w": false, "a": false, "s": false, "d": false};
     
   const cube = useRef<PcEntity>(null);
+  const plane = useRef<PcEntity>(null);
   
   const onUpdate = (dt: number) => {
-      cube.current?.rotate(10 * dt, 20 * dt, 30 * dt);
+      // cube.current?.rotate(10 * dt, 20 * dt, 30 * dt);
 
       // Movement of the player
       // TODO handle user strafing (w + d)
-      if (userInputObject["w"]) {
-        cube?.current?.translate(0, 0, -0.5)
-      }
-      if (userInputObject["s"]) {
-        cube?.current?.translate(0, 0, 0.5)
-      }
-      if(userInputObject["a"]) {
-        cube?.current?.translate(-0.5, 0, 0)
-      }
-      if(userInputObject["d"]) {
-        cube?.current?.translate(0.5, 0, 0)
-      }
-      if(userInputObject["Shift"]) {
-        cube?.current?.translate(0, -0.5, 0)
-      }
-      if(userInputObject[" "]) {
-        cube?.current?.translate(0, 0.5, 0)
+      const handlePlayerMovement = () => {
+        if (userInputObject["w"]) {
+          cube?.current?.translate(0, 0, -0.5)
+        }
+        if (userInputObject["s"]) {
+          cube?.current?.translate(0, 0, 0.5)
+        }
+        if(userInputObject["a"]) {
+          cube?.current?.translate(-0.5, 0, 0)
+        }
+        if(userInputObject["d"]) {
+          cube?.current?.translate(0.5, 0, 0)
+        }
+        if(userInputObject["Shift"]) {
+          cube?.current?.translate(0, -0.5, 0)
+        }
+        if(userInputObject[" "]) {
+          cube?.current?.translate(0, 0.5, 0)
+        }
       }
 
-
+      // handlePlayerMovement();
     }
 
 
@@ -67,31 +70,39 @@ function Scene() {
 
     return (
         <>
-            <Entity name="camera" position={[0, 0, 3]}>
+            {/* Ground plane */}
+            <Entity key="ground" position={[0, -0.6, 0]} scale={[100, 0.1, 100]}>
+              <Render type='box'/>
+              <RigidBody type="static" friction={0.5} />
+              <Collision type="box" halfExtents={[5, 0.1, 5]} />
+            </Entity>
+
+            <Entity name="camera" position={[0, 0, 5]}>
                 <Camera clearColor="#8099e6" />
                 <Script script={CameraControls}/>
             </Entity>
-            <Entity name="light" rotation={[45, 0, 0]}>
+            <Entity name="light" rotation={[30, 0, 0]}>
                 <Light type="directional" />
             </Entity>
+
+
             <Entity name="cube" ref={cube}>
                 <Render type="box" />
             </Entity>
+
+            {/* UI */}
+            {/*
+            <div className="overlay">
+              <button onClick={addBox}>Add Box</button>
+            </div> */}
         </>
     );
 }
 
-function SceneHandler() {
-  // console.log("keyboard: ", app.keyboard)
-  return (
-    <Scene />
-  )
-}
-
 export default function PlayCanvasView() {
     return (
-        <Application>
-          <SceneHandler/>
+        <Application usePhysics>
+          <Scene />
         </Application>
     );
 }
