e53e5f4e2b62ae73fe69ad6bdbd74ceb3ce611b4
Leo-Teeuwen <leoteeuwen325@gmail.com>
2026-09-12 21:53:48 -0500

    basic plane

--- app/components/gameInstance.tsx
+++ app/components/gameInstance.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Canvas } from '@react-three/fiber'
-import { CameraControls, OrbitControls } from '@react-three/drei'
+import { CameraControls, OrbitControls, Plane } from '@react-three/drei'
 
 function SpinningBox() {
   return (
@@ -12,12 +12,22 @@ function SpinningBox() {
   )
 }
 
+function FloorPlane() {
+  return (
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+      <planeGeometry args={[10, 10]} />
+      <meshStandardMaterial color="lightblue" side={2} /> {/* side={2} renders both sides */}
+    </mesh>
+  );
+}
+
 export default function GameInstance() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
+        <FloorPlane/>
         <SpinningBox/>      
         <CameraControls/>  
       </Canvas>
